df0edf84d6c411b1af2a15484fb1fcbead28fb7c
non4eel <mr.lowland@gmail.com>
2024-06-15 14:40:34 +0300

    menu added

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/* sticky header */
+window.onscroll = function () { getSticky() };
+
+const header = document.getElementById("header");
+const sticky = header.offsetTop;
+
+function getSticky() {
+    if (window.scrollY > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+/* active menu item */
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.nav-btn');
+const menuLength = menuItem.length
+
+for (let i = 0; i < menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].classList.add("btn-active");
+    }
+}
